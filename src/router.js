5181df08d1217fdb9fcfe22474c505ad3944998d
Symon 🥭 <symon.bla@gmail.com>
2025-03-18 16:30:58 +0100

    Installed Pinia

--- src/router.js
+++ src/router.js
@@ -1,10 +1,12 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Dashboard from "./components/Dashboard.vue";
 import AddItem from "./components/AddItem.vue";
+import Settings from "./components/Settings.vue";
 
 const routes = [
   { path: "/", component: Dashboard },
-  { path: "/add", component: AddItem }
+  { path: "/add", component: AddItem },
+  { path: "/settings", component: Settings }
 ];
 
 export const router = createRouter({
