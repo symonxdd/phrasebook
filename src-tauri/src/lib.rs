e3b88e00c4d55e5f75076f8aa2806f8a187a57b2
Symon 🥭 <symon.bla@gmail.com>
2025-06-11 13:52:15 +0200

    Dev build window title customization

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -5,6 +5,7 @@ mod paths;
 
 use db::init_db;
 use tauri::async_runtime::block_on;
+use tauri::Manager;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
@@ -13,17 +14,20 @@ pub fn run() {
   tauri::Builder::default()
     .plugin(tauri_plugin_fs::init())
     .plugin(tauri_plugin_opener::init())
+    .setup(|app| {
+      let window = app.get_webview_window("main").unwrap();
+      let mut title = "Phrasebook".to_string();
+      if cfg!(debug_assertions) {
+        title.push_str(" (dev)");
+      }
+      window.set_title(&title)?;
+      Ok(())
+    })
     .invoke_handler(tauri::generate_handler![
       commands::queries::dashboard::get_explore_entries,
       commands::queries::common::get_all_languages,
       commands::queries::common::get_entry_stats,
-      paths::get_app_localappdata,
-      // commands::queries::term_queries::get_all_terms,
-      // commands::queries::term_queries::get_term_details,
-      // commands::queries::concept_queries::get_concept_details,
-      // commands::queries::sentence_queries::get_all_sentences,
-      // commands::queries::sentence_queries::get_sentence_details,
-      // commands::queries::fts_queries::search_entries,
+      paths::get_app_localappdata
     ])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
