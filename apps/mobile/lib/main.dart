dc9408a61bb5def06eaf87a1fc92a0b66a577da7
Symon 🥭 <symon.bla@gmail.com>
2025-07-15 00:49:30 +0200

    Simple BottomNavigationBar with 3 main screens

--- apps/mobile/lib/main.dart
+++ apps/mobile/lib/main.dart
@@ -2,6 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:phrasebook_mobile/src/rust/api/simple.dart';
 import 'package:phrasebook_mobile/src/rust/frb_generated.dart';
 
+import 'screens/explore_screen.dart';
+import 'screens/favorites_screen.dart';
+import 'screens/settings_screen.dart';
+
 Future<void> main() async {
   await RustLib.init();
   runApp(const MyApp());
@@ -13,13 +17,56 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: const Text('flutter_rust_bridge quickstart')),
-        body: Center(
-          child: Text(
-            'Action: Call Rust `greet("Tom")`\nResult: `${greet(name: "Tom")}`',
+      debugShowCheckedModeBanner: false, // removes the debug ribbon
+      title: 'Phrasebook',
+      theme: ThemeData(
+        useMaterial3: true, // optional: enables Material Design 3 styling
+      ),
+      home: const MainNavigation(),
+    );
+  }
+}
+
+class MainNavigation extends StatefulWidget {
+  const MainNavigation({super.key});
+
+  @override
+  State<MainNavigation> createState() => _MainNavigationState();
+}
+
+class _MainNavigationState extends State<MainNavigation> {
+  int _selectedIndex = 0;
+
+  static const List<Widget> _screens = <Widget>[
+    ExploreScreen(),
+    FavoritesScreen(),
+    SettingsScreen(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: _screens[_selectedIndex]),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.explore), label: 'Explore'),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite),
+            label: 'Favorites',
           ),
-        ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Settings',
+          ),
+        ],
       ),
     );
   }
